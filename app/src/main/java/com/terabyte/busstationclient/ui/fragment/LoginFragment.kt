e3a39250e83f0e65b1e483b0e9cbc038fe6b67f4
artem <art.holodilov@gmail.com>
2026-05-26 20:36:12 +0300

    Binding objects in Fragment classes

--- app/src/main/java/com/terabyte/busstationclient/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/terabyte/busstationclient/ui/fragment/LoginFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.terabyte.busstationclient.R
+import com.terabyte.busstationclient.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +21,17 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.textCreateAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_from_register_to_login)
+        }
     }
 
 }
--- app/src/main/java/com/terabyte/busstationclient/ui/fragment/RegisterFragment.kt
+++ app/src/main/java/com/terabyte/busstationclient/ui/fragment/RegisterFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.terabyte.busstationclient.R
+import com.terabyte.busstationclient.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +21,13 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+
+        binding.textOrLogIn.setOnClickListener {
+            findNavController().navigate(R.id.action_from_register_to_login)
+        }
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
